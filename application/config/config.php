ba1f8cc2b60143ca967b85fa51cee252d4d5e79c
sayeesh <sayeesh301@gmail.com>
2018-07-12 21:35:54 +0530

    asdasd

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://http://127.0.0.1:8888/affordableartfest/';
+$config['base_url'] = 'http://127.0.0.1:8888/affordableartfest/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/gaaf_vision.php
+++ application/views/gaaf_vision.php
@@ -1,11 +1,43 @@
 <?php include 'common/header.php' ?>
 <style>
-    .margin_bottom_30{
-        margin-bottom:50px; 
-    }
     .margin_tb_20{
         margin-top:20px;
     }
+	.padding_top_20p{
+		padding-top:20%;
+	}
+	.gaaf_vision_image
+	{
+		margin-bottom:50px;
+		padding:0;
+		-webkit-box-shadow: 50px 50px 0px 0px rgba(43,222,227,1);
+		-moz-box-shadow: 50px 50px 0px 0px rgba(43,222,227,1);
+		box-shadow: 50px 50px 0px 0px rgba(43,222,227,1);
+	}
+	.gaaf_vision_image img
+	{
+		-webkit-box-shadow: -8px 8px 10px 0px rgba(187,187,187,1);
+		-moz-box-shadow: -8px 8px 10px 0px rgba(187,187,187,1);
+		box-shadow: -8px 8px 10px 0px rgba(187,187,187,1);
+	}
+	@media (max-width:768px)
+	{
+		.padding_top_20p
+		{
+			padding-top:0;
+		}
+		
+	}
+	@media (max-width:425px)
+	{
+		.gaaf_vision_image
+		{
+			-webkit-box-shadow: 30px 30px 0px 0px rgba(43,222,227,1);
+			-moz-box-shadow: 30px 30px 0px 0px rgba(43,222,227,1);
+			box-shadow: 30px 30px 0px 0px rgba(43,222,227,1);
+		}
+		
+	}
 </style>
 <div class="col-sm-12 col-xs-12 min_height_container">
     <div class="container margin_top_container">
@@ -14,16 +46,14 @@
                 <div class="head_underline">GAAF Vision</div>
             </div>
             <div class="col-md-12 col-xs-12">
-                Museum of Goa launched the annual Goa Affordable Art Fest (GAAF) whose purpose is to provide quality 
-                art at affordable prices with the objective to enthuse the general public to appreciate, connect and collect art.
-            </div>
-            <div class="col-md-12 col-xs-12 margin_bottom_30">
-                <div class="col-md-6 col-xs-12 margin_tb_20">
-                    <img src="<?= base_url('assets/images/folder_2/Image_1.jpg') ?>" width="100%" />
-                </div>
-                <div class="col-md-6 col-xs-12 margin_tb_20">
+				<div class="col-md-6 col-sm-11 col-xs-10 margin_tb_20 gaaf_vision_image">
                      <img src="<?= base_url('assets/images/folder_2/Image_2.jpg') ?>" width="100%" />
                 </div>
+                <div class="col-md-5 col-xs-12 pull-right margin_tb_20 padding_top_20p">
+					Museum of Goa launched the annual Goa Affordable Art Fest (GAAF) whose purpose is to provide quality 
+					art at affordable prices with the objective to enthuse the general public to appreciate, connect and collect art.
+                </div>
+                
             </div>
         </div>
     </div>
